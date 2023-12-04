2c2d7ac8cc33ab246e03f67b64e39a8475125e0f
Alexander-Kolotow <147614805+Alexander-Kolotow@users.noreply.github.com>
2023-12-04 14:19:25 +0000

    Solved Read Data

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,14 @@
-import { places } from "../../../lib/db";
+import Place from "@/db/models/Places";
+import dbConnect from "@/db/models/connect";
 
-export default function handler(request, response) {
-  return response.status(200).json(places);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+
+    const places = await Place.find();
+    return response.status(200).json(places);
+  } else {
+    return response.status(405).json({ message: "No such place!" });
+  }
 }
