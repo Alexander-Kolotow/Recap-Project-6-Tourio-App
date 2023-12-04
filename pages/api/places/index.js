b94805910a932e656f044ca872a6931dfa223e9f
Alexander-Kolotow <147614805+Alexander-Kolotow@users.noreply.github.com>
2023-12-04 15:26:08 +0000

    add post method for create

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -5,10 +5,24 @@ export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
+    try {
+      const places = await Place.find();
+      return response.status(200).json(places);
+    } catch (error) {
+      return response.status(500).json({ error: error.message });
+    }
+  }
 
-    const places = await Place.find();
-    return response.status(200).json(places);
-  } else {
-    return response.status(405).json({ message: "No such place!" });
+  if (request.method === "POST") {
+    try {
+      const placesData = request.body;
+      await Place.create(placesData);
+      return response.status(201).json({ status: "Place created" });
+    } catch (error) {
+      console.log(error);
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
+
+
